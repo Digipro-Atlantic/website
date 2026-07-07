4d8125df68ba386aa2e8a93252865432c36cc96c
Eveshonyanime Ocheche-Alabi <evesho.alabi@gmail.com>
2026-07-07 15:12:29 +0300

    chore: updated Prettier and ESLint configurations, add Tailwind CSS and TanStack Router dependencies, and implement routing structure in the application

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,53 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(["dist"]),
-  {
-    files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            globals: globals.browser,
+        },
+        rules: {
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+            'no-console': 'off',
+            'no-undef': 'off',
+            'no-unused-vars': 'off',
+            'no-unused-expressions': [
+                'error',
+                {
+                    allowShortCircuit: true,
+                    allowTernary: true,
+                    allowTaggedTemplates: true,
+                },
+            ],
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                },
+            ],
+            '@typescript-eslint/no-explicit-any': 'warn',
+            'jsx-quotes': ['error', 'prefer-double'],
+            'react/jsx-no-target-blank': [
+                'error',
+                { enforceDynamicLinks: true },
+            ],
+            'react/jsx-key': 'error',
+            'react/self-closing-comp': 'off',
+            'react/no-danger': 'warn',
+        },
     },
-  },
-]);
+])
